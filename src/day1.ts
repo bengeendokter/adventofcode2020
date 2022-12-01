80953bf2e9aa6c4421845344adfd1973addad2be
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-12-01 20:06:47 +0100

    Add structure for debugging

---
+++ src/day1.ts
@@ -0,0 +1,21 @@
+import * as fs from "fs";
+
+export default function day1()
+{
+  const content: string = fs.readFileSync("input/day1.txt", "utf-8");
+
+  const elves: Array<string> = content.split("\n\n");
+
+  const elvesList: Array<number> = elves
+    .map(line => line.split("\n"))
+    .map(list =>
+    {
+      const numberList: Array<number> = list.map(stringNumber => parseInt(stringNumber));
+      return numberList.reduce((acc, number) => acc + number);
+    })
+    .filter(total => !isNaN(total));
+
+  elvesList.sort((a, b) => b - a);
+
+  console.log(elvesList.slice(0, 3).reduce((acc, number) => acc + number));
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import day from "./day1";
+
+// change import to run new day
+console.log("start");
+day();
+console.log("end");
