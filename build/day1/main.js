57feacc3d9046861af6a2071294eee42b8e499a2
Ben <79012763+benartshogent@users.noreply.github.com>
2022-12-01 14:28:21 +0100

    Add typescript

---
+++ build/day1/main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+// https://adventofcode.com/2022/day/1
+var sayHello = "hello world 3";
+console.log(sayHello);
---
+++ src/day1/main.ts
@@ -0,0 +1,6 @@
+// https://adventofcode.com/2022/day/1
+const sayHello: string = "hello world 3";
+
+console.log(sayHello);
+
+export { };
