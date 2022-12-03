53646783c0750c0a9dba88af2d1fa148820bb470
bengeendokter <benifit.arts@gmail.com>
2022-12-03 09:52:51 +0100

    Add inpit day3

--- src/day3.ts
+++ src/day3.ts
@@ -2,7 +2,9 @@ import * as fs from "fs";
 
 export default function day(): any
 {
-  const fileContent: string = fs.readFileSync("input/day3.txt", "utf-8");
+	const fileContent: string = fs.readFileSync("input/day3.txt", "utf-8");
 
-  return 0;
+	
+
+	return 0;
 }
