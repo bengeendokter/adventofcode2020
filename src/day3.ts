73ab9fcc5f13f8a2f035aa80dcc6f1b8b1f2fe88
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-12-03 11:04:31 +0100

    Solve part 2 day3

--- src/day3.ts
+++ src/day3.ts
@@ -19,18 +19,39 @@ export default function day(): number
     }
   }
 
+  function groupByN(rsList: string[], n: number = 3): string[][]
+  {
+    let result: string[][] = [];
+
+    for (let index = 0; index < rsList.length; index += n)
+    {
+      result.push(rsList.slice(index, index + n));
+    }
+
+    return result;
+  }
+
   const rucksackList: string[] = fileContent
     .split("\n")
     .map(line => line.trim())
     .filter(line => line.length !== 0);
 
-  const rucksackHalvesList: string[][] = rucksackList
-    .map(rs => [rs.slice(0, rs.length / 2), rs.slice(rs.length / 2, rs.length)]);
+  // part 1
+  // const rucksackHalvesList: string[][] = rucksackList
+  //   .map(rs => [rs.slice(0, rs.length / 2), rs.slice(rs.length / 2, rs.length)]);
 
-  const wrongItemList: string[] = rucksackHalvesList
-    .map(([halve1, halve2]) => [...halve1].find(char => halve2.includes(char)));
+  // const wrongItemList: string[] = rucksackHalvesList
+  //   .map(([halve1, halve2]) => [...halve1].find(char => halve2.includes(char)));
 
-  const priorityList: number[] = wrongItemList.map(charToPriority);
+  // const priorityList: number[] = wrongItemList.map(charToPriority);
+  // return priorityList.reduce((total, priority) => total + priority);
 
+  // part 2
+  const elfGroupList: string[][] = groupByN(rucksackList);
+
+  const badgeList: string[] = elfGroupList
+  .map(([g1, g2, g3]) => [...g1].find(char => g2.includes(char) && g3.includes(char)));
+
+  const priorityList: number[] = badgeList.map(charToPriority);
   return priorityList.reduce((total, priority) => total + priority);
 }
