bafa58d61bcb8385035dd68fb1b6d48abb7a542e
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-12-07 10:15:03 +0100

    Setup day7

---
+++ src/day7.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs";
+import { splitByNewline, sum } from "./util.js";
+
+export default function day7(): number
+{
+  console.log("day");
+  const fileContent: string = fs.readFileSync("input/day7test.txt", "utf-8");
+
+  
+
+  return 0;
+}
